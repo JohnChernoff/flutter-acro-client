853d797128838d70bb342d64dbcd761eb70ad423
John Chernoff <jachernoff@gmail.com>
2025-07-01 10:31:59 -0700

    Initial commit

---
+++ lib/game.dart
@@ -0,0 +1,11 @@
+import 'package:zugclient/zug_area.dart';
+import 'game_model.dart';
+
+class Game extends Area {
+  Game(super.data);
+
+  void update(dynamic data,GameModel? client) {
+    //TODO: add stuff here
+  }
+
+}
